709580dd717cb5b146c1188e656ac3e8a6a56cb6
lyt2015 <lyt2015@users.nonreply.github.com>
2018-09-09 02:20:48 +0800

    Finish connection to front-end

--- server.js
+++ server.js
@@ -2,81 +2,80 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'smart-brain-admin',
+    password: '78952',
+    database: 'smart-brain-db',
+  },
+})
 
 const salt = bcrypt.genSaltSync(10)
 console.log('salt:', salt)
 
-const port = 8080
+const port = 3000
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
-
-const database = {
-  users: [
-    {
-      _id: '001',
-      name: 'Lisa',
-      email: 'lisa@gmail.com',
-      password: '123456',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      _id: '002',
-      name: 'Mary',
-      email: 'mary@gmail.com',
-      password: '123456',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      _id: '003',
-      name: 'aaa',
-      email: 'a@a',
-      password: 'aaaaaaaaa',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-
-  logins: [
-    // {
-    //   _id: '001',
-    //   email: 'lisa@gmail.com',
-    //   hash: '',
-    // },
-  ],
-}
 
 const prettyJSON = obj => {
   return JSON.stringify(obj, null, 2)
 }
 
 // / ==> res = OK
-app.get('/', (req, res) => {
-  // res.send('<h1>Holy Shiiiiiiiiiiiit</h1>')
-  res.json(database.users)
+app.get('/', async (req, res) => {
+  const users = await knex.select().from('users')
+  const logins = await knex.select().from('logins')
+
+  const db = { users, logins }
+
+  res.json(db)
 })
 
 // /signin ==> POST = success/fail
-app.post('/signin', (req, res) => {
+app.post('/signin', async (req, res) => {
   const { email, password } = req.body
-  let foundUser = null
-  database.users.some(user => {
-    if (email === user.email) {
-      if (password === user.password) {
-        foundUser = user
-        return true
-      }
-    }
-  })
 
-  if (foundUser) {
-    console.log('user found')
-    res.status(200).json(foundUser)
+  if (email && password) {
+    try {
+      const login = (await knex
+        .from('logins')
+        .where({ email })
+        .select('email', 'hash'))[0]
+
+      // console.log(login)
+
+      if (login) {
+        bcrypt.compare(password, login.hash, async (err, result) => {
+          if (err) {
+            console.log(err)
+            return res.status(500).json('something went wrong')
+          }
+
+          if (result === true) {
+            // console.log('correct password')
+            const user = (await knex
+              .from('users')
+              .where({ email })
+              .select('*'))[0]
+            // console.log(user)
+            res.json(user)
+          } else {
+            // console.log('wrong pasword')
+            res.status(400).json('Please check login information again.')
+          }
+        })
+      } else {
+        return res.status(400).json('Please check login information again.')
+      }
+    } catch (error) {
+      console.log(error)
+      return res.status(400).json('Please check login information again.')
+    }
   } else {
-    console.log('user not found')
-    res.status(400).json('fail')
+    res.status(400).json('Please fill in both email and password.')
   }
 })
 
@@ -84,85 +83,78 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
   const { email, password, name } = req.body
   if (email && password && name) {
-    const _id = (database.users.length + 1).toString().padStart(3, '0')
-
-    let newLogin = null
-    let newUser = null
     bcrypt.hash(password, salt, null, (err, hash) => {
       if (err) {
         console.log(err)
-        res.status(400).json('fail')
-        // res.send('Sorry, there are some problems.')
+        return res.status(400).json('something went wrong')
       }
 
       if (hash) {
-        console.log('hash:', hash)
-      }
-      newLogin = {
-        _id,
-        email,
-        hash,
-      }
-      database.logins.push(newLogin)
-      console.log(newLogin)
+        const newUser = { name, email, joined: new Date() }
+        const newLogin = { email, hash }
 
-      newUser = {
-        _id,
-        email,
-        name,
-        entries: 0,
-        joined: new Date(),
-      }
-      database.users.push(newUser)
-      console.log(newUser)
+        knex
+          .transaction(trx => {
+            return trx
+              .insert(newLogin)
+              .into('logins')
+              .then(() => {
+                return trx
+                  .insert(newUser)
+                  .into('users')
+                  .returning('*')
+              })
+          })
+          .then(insertedUsers => {
+            // console.log('insertedUsers:', insertedUsers)
 
-      // res.setHeader('Content-Type', 'application/json')
-      // res.writeHead(200)
-      // res.write('You are now a registered user\n')
-      // res.write(prettyJSON(newUser))
-      // res.write(prettyJSON(newLogin))
-      // return res.end()
-      res.status(200).json(newUser)
+            return res.json(insertedUsers[0])
+          })
+          .catch(err => {
+            return res.status(400).json('Please try register with other name or email again.')
+          })
+      }
     })
+  } else {
+    res.status(400).json('Please register with name, email and password.')
   }
 })
 
 // /profile/:userId ==> GET = user
-app.get('/profile/:userId', (req, res) => {
-  const userId = req.params.userId
-  let foundUser = null
-  database.users.some(user => {
-    if (userId === user._id) {
-      return (foundUser = user)
-    }
-  })
+app.get('/profile/:userId', async (req, res) => {
+  const { userId } = req.params
+  try {
+    const user = (await knex('users')
+      .select('*')
+      .where({ _id: userId }))[0]
 
-  if (foundUser) {
-    res.json(foundUser)
-  } else {
-    res.status(404).send('No user found.')
+    if (user) {
+      res.json(user)
+    } else {
+      res.status(400).send('no user found')
+    }
+  } catch (error) {
+    res.status(500).json('retrieving data failed')
   }
 })
 
 // /imgage ==> PATCH --> user
-app.patch('/image', (req, res) => {
+app.patch('/image', async (req, res) => {
   const { _id } = req.body
-  let foundUser = null
-  database.users.some(user => {
-    if (_id === user._id) {
-      user.entries += 1
-      return (foundUser = user)
-    }
-  })
 
-  if (foundUser) {
-    res.json({
-      _id: foundUser._id,
-      entries: foundUser.entries,
-      name: foundUser.name,
-    })
-  } else {
-    res.status(404).send('No user found.')
+  try {
+    const user = (await knex('users')
+      .where({ _id })
+      .increment('entries', 1)
+      .returning('*'))[0]
+
+    if (user) {
+      return res.json(user)
+    } else {
+      return res.status(400).json('failed updating user')
+    }
+  } catch (error) {
+    res.status(500).json('retrieving data failed')
   }
 })
 
